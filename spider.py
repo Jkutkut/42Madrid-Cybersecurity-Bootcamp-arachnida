e2567357314e42c22e5454bc7542fca125ff7472
Jkutkut <jorgeregonzalez@gmail.com>
2022-04-25 19:29:01 +0200

    dependencies fixed

--- spider.py
+++ spider.py
@@ -7,9 +7,10 @@ if __name__ == '__main__':
 	datos = urllib.request.urlopen(url).read().decode()
 
 	from bs4 import BeautifulSoup
+
 	soup =  BeautifulSoup(datos)
 	tags = soup("img")
 	for tag in tags:
 		print(tag.get("src"))
-	print(datos)
+	# print(datos)
 
