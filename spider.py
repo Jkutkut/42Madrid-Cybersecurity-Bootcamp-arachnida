47618d519bfcc150988103b13c8173dd8f49a604
Jkutkut <jorgeregonzalez@gmail.com>
2022-04-25 19:22:20 +0200

    dependencies needed

---
+++ spider.py
@@ -0,0 +1,15 @@
+import sys
+
+if __name__ == '__main__':
+	import urllib.request
+	# url = sys.argv[0]
+	url = "https://github.com/jkutkut"
+	datos = urllib.request.urlopen(url).read().decode()
+
+	from bs4 import BeautifulSoup
+	soup =  BeautifulSoup(datos)
+	tags = soup("img")
+	for tag in tags:
+		print(tag.get("src"))
+	print(datos)
+
