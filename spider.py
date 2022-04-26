b0c74f7752ce601d002c5bcc2ebf1ea763327bf5
Jkutkut <jorgeregonzalez@gmail.com>
2022-04-26 09:37:45 +0200

    migrating code to class

--- spider.py
+++ spider.py
@@ -4,58 +4,67 @@ import sys
 import urllib.request
 from bs4 import BeautifulSoup
 
-DEFAULT_PATH = "./idea/"
-DEFAULT_RECURSIVE = 5
+class Arachnida:
 
-def spider(url, recursive=0, path=DEFAULT_PATH):
-	datos = urllib.request.urlopen(url).read().decode()
-	soup =  BeautifulSoup(datos)
-	tags = soup("img")
-	for tag in tags:
-		print(tag.get("src"))
+	DEF_PATH = "./idea/"
+	DEF_DEPTH = 5
+
+	def __init__(self, argc, argv):
+		self.url = argv[argc]
+		self.path = self.DEF_PATH
+		self.depth = 0
+		self.update(argc, argv)
+
+	def update(self, argc, argv):
+		i = 1
+		while i < argc - 1:
+			arg = sys.argv[i]
+			if arg == '-r':
+				if i < argc - 2:
+					if sys.argv[i + 1] == '-l':
+						if i < argc - 3 and sys.argv[i + 2].isdigit():
+							recursive = int(sys.argv[i + 2])
+						else:
+							print("-l must be followed by a number")
+							exit()
+						i += 2
+				else:
+					recursive = self.DEF_DEPTH
+			elif arg == '-p':
+				if i < argc - 2:
+					path = sys.argv[i + 1]
+				else:
+					print("-p must be followed by a path")
+					exit()
+				i += 1
+			else:
+				print("Flag not recognized")
+				print(f'\t{arg}')
+				exit()
+			i += 1
+
+	def run(self):
+		Arachnida.spider(self.url, self.path, self.depth)
+
+	def __str__(self) -> str:
+		return f'Arachnida:\n\tURL: {self.url}\n\tPath: {self.path}\n\tDepth: {self.depth}'
+
+	@staticmethod
+	def spider(cls, url, recursive=0, path=None):
+		if path is None:
+			cls.DEF_PATH
+		datos = urllib.request.urlopen(url).read().decode()
+		soup =  BeautifulSoup(datos)
+		tags = soup("img")
+		for tag in tags:
+			print(tag.get("src"))
+
 
 
 if __name__ == '__main__':
 	if len(sys.argv) == 1:
 		print("Usage: ./spider [-rlpS] URL")
 		exit()
-	i = 1
-	recursive=0
-	path=DEFAULT_PATH
-	while i < len(sys.argv) - 1:
-		arg = sys.argv[i]
-		if arg == '-r':
-			if i < len(sys.argv) - 2:
-				if sys.argv[i + 1] == '-l':
-					if i < len(sys.argv) - 3 and sys.argv[i + 2].isdigit():
-						recursive = int(sys.argv[i + 2])
-					else:
-						print("-l must be followed by a number")
-						exit()
-					i += 2
-			else:
-				recursive = DEFAULT_RECURSIVE
-		elif arg == '-p':
-			if i < len(sys.argv) - 2:
-				path = sys.argv[i + 1]
-			else:
-				print("-p must be followed by a path")
-				exit()
-			i += 1
-		else:
-			print("Flag not recognized")
-			print(f'flag: {arg}')
-			print(f'Recursive: {recursive}')
-			print(f'Path: {path}')
-			exit()
-		i += 1
-
-	url = "https://github.com/jkutkut" # TODO Debug
-	# url = sys.argv[i]
-
-	# spider(url)
-
-	print(f'Recursive: {recursive}')
-	print(f'Path: {path}')
-	print(f'URL: {url}')
+	a = Arachnida(len(sys.argv) - 1, sys.argv)
+	print(a)
 
