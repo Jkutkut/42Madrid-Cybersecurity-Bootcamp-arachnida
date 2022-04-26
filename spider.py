7169fbe2c0b0fd5f9d2c166d80086580e3eadca1
Jkutkut <jorgeregonzalez@gmail.com>
2022-04-26 09:15:36 +0200

    error management added to args

--- spider.py
+++ spider.py
@@ -4,8 +4,8 @@ import sys
 import urllib.request
 from bs4 import BeautifulSoup
 
-DEFAULT_PATH="./idea/"
-DEFAULT_RECURSIVE=5
+DEFAULT_PATH = "./idea/"
+DEFAULT_RECURSIVE = 5
 
 def spider(url, recursive=0, path=DEFAULT_PATH):
 	datos = urllib.request.urlopen(url).read().decode()
@@ -21,14 +21,41 @@ if __name__ == '__main__':
 		exit()
 	i = 1
 	recursive=0
-	while i < len(sys.argv):
+	path=DEFAULT_PATH
+	while i < len(sys.argv) - 1:
 		arg = sys.argv[i]
-		print(f'{str(i)} {str(len(sys.argv) - 2)} {arg}')
 		if arg == '-r':
-			recursive = DEFAULT_RECURSIVE
+			if i < len(sys.argv) - 2:
+				if sys.argv[i + 1] == '-l':
+					if i < len(sys.argv) - 3 and sys.argv[i + 2].isdigit():
+						recursive = int(sys.argv[i + 2])
+					else:
+						print("-l must be followed by a number")
+						exit()
+					i += 2
+			else:
+				recursive = DEFAULT_RECURSIVE
+		elif arg == '-p':
+			if i < len(sys.argv) - 2:
+				path = sys.argv[i + 1]
+			else:
+				print("-p must be followed by a path")
+				exit()
+			i += 1
+		else:
+			print("Flag not recognized")
+			print(f'flag: {arg}')
+			print(f'Recursive: {recursive}')
+			print(f'Path: {path}')
+			exit()
 		i += 1
 
 	url = "https://github.com/jkutkut" # TODO Debug
 	# url = sys.argv[i]
 
 	# spider(url)
+
+	print(f'Recursive: {recursive}')
+	print(f'Path: {path}')
+	print(f'URL: {url}')
+
